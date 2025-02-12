7735f36a33e7ca25a46101caa72f46e0debb620b
eraydiler <eray.diler@gmail.com>
2025-02-12 03:41:22 +0300

    Handled exceptions and displayed on UI elegantly.

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,4 +1,10 @@
-OUTPUT_DIR = "output"
-TEMP_PATH = "output/temp_uploaded_file.csv"
-REPORT_NAME = "vergi_hesaplama_raporu.csv"
-REPORT_PATH = f'output/{REPORT_NAME}'
+from pathlib import Path
+
+# Get the root directory of the project
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Define paths
+OUTPUT_DIR = BASE_DIR / 'output'
+TEMP_PATH = BASE_DIR / 'temp_uploaded_file.csv'
+REPORT_PATH = OUTPUT_DIR / 'vergi_hesaplama_raporu.csv'
+REPORT_NAME = 'vergi_hesaplama_raporu.csv'
